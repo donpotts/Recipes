d3e6a15c4691ad70242fbcbaa8cbb6821adbfa26
Don Potts <don.potts@beverycreative.com>
2024-04-25 10:49:27 -0500

    Added Pointer cursor for cardrecipes

--- BlazorRecipes/Program.cs
+++ BlazorRecipes/Program.cs
@@ -167,11 +167,14 @@ builder.Services.AddScoped<ImageService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseForwardedHeaders();
